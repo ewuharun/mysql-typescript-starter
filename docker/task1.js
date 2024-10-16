af24c1eff568ef791460df81cade1e599806bbe0
ewuharun <ewu.harun@gmail.com>
2024-10-16 15:39:01 +0600

    add database

--- docker/task1.js
+++ docker/task1.js
@@ -1,15 +1,27 @@
-const axios = require('axios');
-
+const axios = require("axios");
+const mysql = require("mysql2/promise");
 
 async function fetchAPI1() {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts'); // Replace with actual API
-    console.log('API 1 Response:');
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    ); // Replace with actual API
+    console.log("API 1 Response:");
+
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      database: "test",
+    });
+
+    const [results, fields] = await connection.query("SELECT 1");
+
+    console.log("result", results);
   } catch (error) {
-    console.error('Error fetching API 1:', error.message);
+    console.error("Error fetching API 1:", error.message);
   }
 }
 
 setInterval(() => {
-    fetchAPI1()
+  fetchAPI1();
 }, 10000);
